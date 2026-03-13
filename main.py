71b089f73218445fd2653e6608bbe109707a9854
Daniel Delgado <qrn6@txstate.edu>
2026-03-13 12:49:32 -0500

    added evaluation_data_frame allows ability to predict on w/o including 2025 in training

--- main.py
+++ main.py
@@ -5,14 +5,29 @@ def main() -> None:
     """
     Script to build the training dataset.
     """
-    data_frame = build_training_dataset(
+    training_data_frame = build_training_dataset(
         seasons=[2021, 2022, 2023, 2024],
         min_games_for_player=4,
         output_path="data/processed/wr_training_dataset_2021-2024.csv",
     )
-    print(data_frame.head())
-    print(f"\nRows: {data_frame.shape[0]}")
-    print(f"Cols: {data_frame.shape[1]}")
+
+    """
+    Evaluation / Prediction data 2025 season
+    Same filters as training data, but only 2025 season
+    """
+    evaluation_2025_data_frame = build_training_dataset(
+        seasons=[2025],
+        min_games_for_player=4,
+        output_path="data/processed/wr_evaluation_dataset_2025.csv",
+    )
+
+    print(training_data_frame.head())
+    print(f"\nRows: {training_data_frame.shape[0]}")
+    print(f"Cols: {training_data_frame.shape[1]}")
+
+    print(evaluation_2025_data_frame.head())
+    print(f"\nRows: {evaluation_2025_data_frame.shape[0]}")
+    print(f"Cols: {evaluation_2025_data_frame.shape[1]}")
 
 
 if __name__ == "__main__":
